2cf2a1e4bcba22f80b83563a40d1e591a34b05f7
Emineabayeva <abayevaemine01@gmail.com>
2025-04-06 11:13:01 +0400

    backend

--- backend/app.js
+++ backend/app.js
@@ -4,5 +4,15 @@
  const app = express()
 
  import dotenv from "dotenv"
+import { connectDatabase } from "./config/dbConnect.js"
 
- dotenv.config({path:"config/confing.env"})
+ dotenv.config({path:"config/config.env"})
+
+
+ connectDatabase()
+
+//  app.get('/')
+
+ app.listen(process.env.PORT,()=>{
+console.log(` PORT dinlenilir ${process.env.PORT} ve ${process.env.NODE_ENV } `)
+ })
