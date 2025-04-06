b57facf0dd524a26a802da3c24b222b6f34899ec
Emineabayeva <abayevaemine01@gmail.com>
2025-04-06 12:45:27 +0400

    mehsullar geldi

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,22 @@
- 
- import express from "express"
 
- const app = express()
-
- import dotenv from "dotenv"
+import express, { Router } from "express"
+import dotenv from "dotenv"
 import { connectDatabase } from "./config/dbConnect.js"
 
- dotenv.config({path:"config/config.env"})
+const app = express()
 
+// Routelarin daxil edilmesi 
+import productRoutes from "./routes/product.js"
 
- connectDatabase()
+dotenv.config({ path: "config/config.env" })
+connectDatabase()
+
+app.use(express.json())
+
+app.use("/api/v1",productRoutes)
 
 //  app.get('/')
 
- app.listen(process.env.PORT,()=>{
-console.log(` PORT dinlenilir ${process.env.PORT} ve ${process.env.NODE_ENV } `)
- })
+app.listen(process.env.PORT, () => {
+    console.log(` PORT dinlenilir ${process.env.PORT} ve ${process.env.NODE_ENV} `)
+})
---
+++ backend/controllers/productController.js
@@ -0,0 +1,13 @@
+import Product from "../models/Product.js";
+
+export const getProducts = async(req,res) =>{
+  const products =await Product.find()
+
+  res.status(200).json({
+    products
+  })
+}
+
+export const addProducts = async (req,res) =>{
+
+}
